ba8fc1c22d4edef7ef0714292c55f6294e94dee2
Maxim Startsev <m.startsev@astondevs.ru>
2023-08-30 19:48:27 +0300

    Initial Commit

---
+++ Hotel Booking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel Booking
+//
+//  Created by Maxim Startsev on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
